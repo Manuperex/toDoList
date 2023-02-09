a2751f19956f5f34486b81b61738713e40a11294
ruthyi <jbruthlizcano@gmail.com>
2023-02-09 16:28:08 -0500

    merge develop and feature 2

--- my-react-app/src/components/Form.jsx
+++ my-react-app/src/components/Form.jsx
@@ -1,64 +1,27 @@
 import React from "react";
 import { useState } from "react";
-import {
-  mediaQueryTablet,
-  mediaQuerymobile,
-  mediaQueryDesktop,
-} from "../mediaQuery";
+import { mediaQueryTablet, mediaQuerymobile, mediaQueryDesktop } from "../mediaQuery"
 
 const Form = ({ addTodo }) => {
-  const [inputValue, setInputValue] = useState("");
-  const [id, setID] = useState();
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    if (inputValue.trim() === "") return;
-
-    addTodo({ title: inputValue, completed: false });
-    let salida={"title": inputValue, "completed": false }
-    console.log(typeof salida);
-    console.log(salida);
-    const  body= JSON.stringify(salida);
-    const requestInit = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: body
+    const [inputValue, setInputValue] = useState("");
+    const [id, setID] = useState()
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
     };
-    console.log(requestInit);
-    async function fetchData() {
-      // const response = await fetch('https://backend-todolistdos.onrender.com/todos');
-      const response = await fetch(
-        "https://backend-todolistdos.onrender.com/todos/new",
-        requestInit
-      );
-      const data = await response.json(); //Convierte la respuesta en un archivo json
-      console.log(data);
-      // console.log(data[0]._id);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(inputValue.trim() ==="") return;
+
+        addTodo({ title: inputValue, completed: false, id });
+        setInputValue("");
     }
-
-    fetchData();
-    setInputValue("");
-  };
-
   return (
     <form className="ui form" onSubmit={handleFormSubmit}>
       <div className="ui grid center aligned">
         <div className="row">
-          <div
-            className={
-              mediaQuerymobile
-                ? "column middle aligned eight wide"
-                : mediaQueryTablet
-                ? "column middle aligned nine wide"
-                : mediaQueryDesktop
-                ? "column middle aligned nine wide"
-                : "column middle aligned nine wide"
-            }
-          >
+          <div className={mediaQuerymobile ? "column middle aligned eight wide" : mediaQueryTablet ? "column middle aligned nine wide" : mediaQueryDesktop? "column middle aligned nine wide" : "column middle aligned nine wide"}>
             <input
               value={inputValue}
               onChange={handleInputChange}
@@ -67,24 +30,8 @@ const Form = ({ addTodo }) => {
             />
           </div>
           <div className="column middle aligned two wide ">
-            <button
-              id="btn-input"
-              type="submit"
-              className={
-                mediaQuerymobile
-                  ? "ui large button circular icon"
-                  : "ui huge button circular icon"
-              }
-            >
-              <i
-                className={
-                  mediaQuerymobile
-                    ? "large white plus square outline icon"
-                    : mediaQueryTablet
-                    ? "big white plus square outline icon"
-                    : "huge white plus square outline icon"
-                }
-              ></i>
+            <button id="btn-input" type="submit" className={mediaQuerymobile ? "ui large button circular icon" : "ui huge button circular icon"}>
+              <i className={mediaQuerymobile ? "large white plus square outline icon" : mediaQueryTablet ? "big white plus square outline icon" : "huge white plus square outline icon"}></i>
             </button>
           </div>
         </div>
